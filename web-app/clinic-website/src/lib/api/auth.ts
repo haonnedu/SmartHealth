0cf55c3708f84419c6cc5aa876471424fa03962a
haonnedu <haonn.edu.@gmail.com>
2025-03-23 19:56:38 +0700

    fix build

--- web-app/clinic-website/src/lib/api/auth.ts
+++ web-app/clinic-website/src/lib/api/auth.ts
@@ -1,8 +1,7 @@
-// lib/api/auth.ts
-import { LoginRequest, LoginResponse } from "@/types/api-types";
 import axios from "@/lib/axios"; // đã setup baseURL + interceptors nếu có
+import { LoginRequest } from "./types/type.gen";
 
-export const login = async (payload: LoginRequest): Promise<LoginResponse> => {
-  const res = await axios.post<LoginResponse>("/api/auth/login", payload);
+export const login = async (payload: LoginRequest): Promise<any> => {
+  const res = await axios.post("/api/auth/login", payload);
   return res.data;
 };
