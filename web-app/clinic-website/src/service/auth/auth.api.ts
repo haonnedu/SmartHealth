bf88afc017fd979ba71139f88664c0c7ff606d46
Hao Nguyen <haonn.edu.@gmail.com>
2025-05-27 00:15:08 +0700

    fix follow

--- web-app/clinic-website/src/service/auth/auth.api.ts
+++
@@ -1,8 +0,0 @@
-// service/auth/auth.api.ts
-
-import { LoginRequest } from "@/lib/api/types/type.gen";
-import axiosInstance from "@/lib/axios-instance";
-
-export const postAuthLogin = (data: LoginRequest) => {
-  return axiosInstance.post("/api/auth/login", data);
-};
